63ca595492199eb772d210649ed7923d1359aaf8
Radhitya Hafif <email.anda@contoh.com>
2026-08-27 16:31:47 +0700

    Update Dock components and add window store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,8 +4,10 @@ import gsap from "gsap";
 
 import { dockApps } from "#constants/index.js";
 import { useGSAP } from "@gsap/react";
+import useWindowStore from "#store/window.js";
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -59,6 +61,19 @@ const Dock = () => {
 
     const toggleApp = (app) => {
         // TODO Implement Open Window Logic
+        if (!app.canOpen) return;
+
+        const window = windows[app.id];
+
+        if (!window) {
+            console.error(`Window not founf for app: ${app.id}`);
+        }
+
+        if (window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
     };
 
     return <section id="dock">
