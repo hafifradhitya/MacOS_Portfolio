b2310858b2a6547de8bde315f0043d90368203e9
Radhitya Hafif <email.anda@contoh.com>
2026-08-27 18:58:27 +0700

    wip: window wrapper setup

---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,9 @@
+const WindowWrapper = (Component, windowKey) => {
+    const Wrapped = (props) => {
+        const { focusWindow, windows } = useWindowStore();
+    };
+
+    return <div>WindowWrapper</div>;
+};
+
+export default WindowWrapper;
